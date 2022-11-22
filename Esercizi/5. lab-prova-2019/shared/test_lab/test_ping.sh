966ceae689216f76d567a2e220e41c3ce2f1bdb9
mgranchelli <manuelgranchelli@gmail.com>
2022-11-22 10:19:28 +0100

    added lab prova intermedia 2019

---
+++ Esercizi/5. lab-prova-2019/shared/test_lab/test_ping.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+awk '{print $1}' < $1 | while read ip; do
+    if ping -c1 $ip >/dev/null 2>&1; then
+        printf "%-20s IS UP\n" $ip
+    else
+        echo ------- $ip IS DOWN -------
+    fi
+done
